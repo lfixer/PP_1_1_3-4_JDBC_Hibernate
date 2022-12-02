b0661509974fb66a9e9f60c118528296f54382fd
lfixer <strmgddn@gmail.com>
2022-12-03 01:09:14 +0300

    1.1.2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.Connection;
+import java.sql.Driver;
+
+
 public class Main {
+    private final static UserService service = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        service.createUsersTable();
+
+        service.saveUser("A", "A", (byte) 1);
+        service.saveUser("B", "B", (byte) 2);
+        service.saveUser("C", "C", (byte) 3);
+        service.saveUser("D", "D", (byte) 4);
+
+        service.removeUserById(2);
+        service.getAllUsers();
+        service.cleanUsersTable();
+        service.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/users_db";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "1754603!11";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        }
+        catch (Exception e) {
+            System.out.println("Не удалось создать подключение к БД");
+        }
+        return connection;
+    }
+
+
+
 }
